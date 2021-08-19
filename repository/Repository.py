5cf755bee4c44c00db58a9c45dd9ef53b63a13a6
AZCONA, MARCOS <marcosazcona2000@hotmail.com>
2021-08-18 22:50:24 -0300

    Agregada alta de materia

--- repository/Repository.py
+++ repository/Repository.py
@@ -8,10 +8,7 @@ class Repository(object):
     def exists_subject(self, subject_name):
         result = self._session.query(Subject).filter(Subject.name == subject_name).all()
         print(result)
-        return False
-        #query = 'SELECT * FROM `Materia` WHERE `nombre` = (%s)'
-        #result = self._db.execute_with_results(query, (subject_name,)) 
-        #return len(result) == 1
+        return len(result) == 1
 
     def add_subject(self,subject_name, semester_number: int):
         subject = Subject(name = subject_name, semester_number = semester_number)
